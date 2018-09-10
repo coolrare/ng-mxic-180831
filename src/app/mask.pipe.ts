d018692626633a60afb8e5e12813732da35cbf2d
Will <doggy.huang@gmail.com>
2018-09-10 15:05:06 +0800

    自訂 Pipe 元件

--- src/app/mask.pipe.ts
+++ src/app/mask.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MaskPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, max: any = 5): any {
+    // let max = 5;
+    if (value.length <= 5) {
+      return value;
+    } else {
+      return value.substr(0, max) + '***';
+    }
   }
 
 }
