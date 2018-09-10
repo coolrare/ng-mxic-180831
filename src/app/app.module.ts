9ba724c88ff692a95cb2eaac8c4c320a543dec05
Will <doggy.huang@gmail.com>
2018-09-10 15:20:16 +0800

    匯入 HttpClientModule 到 AppModule 裡面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,6 +19,7 @@ import { MaskPipe } from './mask.pipe';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
