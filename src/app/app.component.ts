f2dab9221aee721188d207b2aca3d69868ec3d24
Will <doggy.huang@gmail.com>
2018-09-10 09:50:41 +0800

    示範範本參考變數用法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'demo1';
   keyword = '123';
+  changeKeyword(value: string) {
+    this.keyword = value;
+  }
+  changeTitle(value: HeaderComponent) {
+    value.title = this.keyword;
+  }
 }
